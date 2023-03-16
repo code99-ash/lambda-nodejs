9fc8625b96e29ec525d2c9ae00a72c5bf4c94b76
code99-ash <oyetikh1@gmail.com>
2023-03-16 21:33:56 +0000

    json parser implementation

--- src/addTodo.js
+++ src/addTodo.js
@@ -1,15 +1,15 @@
 const { v4 } = require("uuid")
 const AWS = require('aws-sdk')
+const middy = require('@middy/core')
+const httpJsonBodyParser = require('@middy/http-json-body-parser')
 
 const addTodo = async (event) => {
 
   const dynamodb = new AWS.DynamoDB.DocumentClient()
 
-  const { todo } = JSON.parse(event.body)
+  const { todo } = event.body
   const createdAt = new Date().toISOString();
   const id = v4()
-
-  console.log("This is an id", id)
 
   const newTodo = {
     id,
@@ -31,5 +31,5 @@ const addTodo = async (event) => {
 };
 
 module.exports = {
-  handler: addTodo
+  handler: middy(addTodo).use(httpJsonBodyParser())
 }
